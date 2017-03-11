dbb4c242ca7c9b664c40db44d8352dd78b7853d0
Bartek Wajti <bartekwajti@gmail.com>
2017-03-11 17:44:25 +0100

    list with permission checkbox

--- src/Police/Admin/AdminController.java
+++ src/Police/Admin/AdminController.java
@@ -1,20 +1,62 @@
 package Police.Admin;
 
 import Police.Main;
+import Police.model.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
 /**
  * Created by Przemysław on 2017-03-11.
  */
-public class AdminController {
+public class AdminController implements Initializable{
     @FXML
     private Button backButton;
 
+    @FXML
+    TableView<User> userTableView;
+
+    @FXML
+    TableColumn<User, String> userFirstNameColumn;
+
+    @FXML
+    TableColumn<User, String> userLastNameColumn;
+
+    @FXML
+    TableColumn<User, Boolean> adminColumn;
+
+
+    private ObservableList<User> users = FXCollections.observableArrayList();
     public void backToMainMenu(ActionEvent actionEvent) throws Exception {
         Main main = new Main();
         main.start((Stage) backButton.getScene().getWindow());
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        userTableView.setEditable(true);
+        adminColumn.setCellValueFactory(param -> param.getValue().adminProperty());
+        adminColumn.setCellFactory(CheckBoxTableCell.forTableColumn(adminColumn));
+
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("Jan", "Kowalski", true));
+        userList.add(new User("Bozena", "Dykier", false));
+
+        users.addAll(userList);
+        userTableView.setItems(users);
+
     }
 }
---
+++ src/Police/model/User.java
@@ -0,0 +1,64 @@
+package Police.model;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * Created by Admin on 2017-03-11.
+ */
+public class User {
+    private StringProperty firstName;
+    private StringProperty lastName;
+    private BooleanProperty admin;
+
+    public User(String firstName, String lastName, boolean admin) {
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.admin = new SimpleBooleanProperty(admin);
+    }
+
+    public User() {
+        this.firstName = new SimpleStringProperty("da");
+        this.lastName = new SimpleStringProperty("dfs");
+        this.admin = new SimpleBooleanProperty(false);
+    }
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public StringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public StringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public boolean isAdmin() {
+        return admin.get();
+    }
+
+    public BooleanProperty adminProperty() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin.set(admin);
+    }
+
+
+}
