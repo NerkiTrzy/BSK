7cc2acbbb250bee136decdbef4a76ee41fdd798d
NerkiTrzy <przemek.roguski@wp.pl>
2017-03-11 13:48:24 +0100

    Added back to Main Menu from Admin Panel.

--- src/Police/Admin/AdminController.java
+++ src/Police/Admin/AdminController.java
@@ -1,7 +1,20 @@
 package Police.Admin;
 
+import Police.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 /**
  * Created by Przemysław on 2017-03-11.
  */
 public class AdminController {
+    @FXML
+    private Button backButton;
+
+    public void backToMainMenu(ActionEvent actionEvent) throws Exception {
+        Main main = new Main();
+        main.start((Stage) backButton.getScene().getWindow());
+    }
 }
