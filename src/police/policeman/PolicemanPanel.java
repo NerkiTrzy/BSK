7e5828665b3f9868b818ceaf75526e0f02d39250
NerkiTrzy <przemek.roguski@wp.pl>
2017-05-17 20:08:24 +0200

    Try repair project

---
+++ src/police/policeman/PolicemanPanel.java
@@ -0,0 +1,21 @@
+package police.policeman;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Przemysław on 2017-03-13.
+ */
+public class PolicemanPanel extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("policemanWindow.fxml"));
+        primaryStage.setTitle("Panel Policjanta");
+        primaryStage.setScene(new Scene(root, 1024, 768));
+        primaryStage.show();
+    }
+}
